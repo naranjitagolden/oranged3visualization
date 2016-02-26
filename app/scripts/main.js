a02dc4de7566fbb754aaf80fc4ad6cc8a6b1ac71
naranjitagolden <naranja49@gmail.com>
2016-02-26 18:14:24 -0500

    d3 Comenzando con yeoman y toda la cosa, avanzando luego para darle bower install d3 --save, para continuar, y vamos a seguir haber que sucede

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,10 @@
-console.log('\'Allo \'Allo!');
+var sampleSVG = d3.select("#viz")
+        .append("svg")
+        .attr("width", 1000)
+        .attr("height", 500);    
+
+    sampleSVG.append("circle")
+    	.attr("class", "middle-circle")
+        .attr("r", 40)
+        .attr("cx", 700)
+        .attr("cy", 300);
