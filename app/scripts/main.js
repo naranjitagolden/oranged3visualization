c211794225f8e3654e6d7229432f5f277c39d65e
naranjitagolden <naranja49@gmail.com>
2016-03-07 16:26:46 -0500

    Avanzando en Y Ahora buscando modificar el eje Y a través e Math buscando mover los circulos en diferentes direcciones sinembargo, todavía el cambio es muy pequeño y no logra percibirse

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,12 +14,15 @@ var circles = svg.selectAll("circle")
             .enter()
             .append("circle");
 
-var cx = Math.PI / 4
+var cx = Math.PI / 5
+var cy = Math.PI / 2
 
 circles.attr("cx", function(d, i) {
     return (i * cx) + 25;
 })
-        .attr("cy", h/2)
+        .attr("cy", function(d, i){
+            return (i * cy) + 20
+        })
         .attr("r", function(d) {
             return d
         })
