8ef10a58a9617742f92d40193bfaa2f5ade6a2ac
naranjitagolden <naranja49@gmail.com>
2016-03-06 19:51:30 -0500

    ultimos cambios en analytic viernes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,20 @@
-var sampleSVG = d3.select("#viz")
+// espacio svg
+var espacio = d3.select("#viz")
         .append("svg")
         .attr("width", 1000)
-        .attr("height", 500);    
+        .attr("height", 500)    
 
- var circle =    sampleSVG.append("circle")
+//circulo central
+ var centro =  espacio.append("circle")
     	.attr("class", "middle-circle")
         .attr("r", 40)
         .attr("cx", 700)
         .attr("cy", 300);
+
+//circunsferencia
+var linea  = espacio.append("circle")
+	.attr("class", "around")
+	.attr("r", 200)
+	.attr("cx", 700)
+	.attr("cy", 300);
+	
