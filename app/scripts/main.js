fb7785aa67dd65b57b486d3a1044d995e448f555
naranjitagolden <naranja49@gmail.com>
2016-03-09 15:05:41 -0500

    Que sucede acá :) Buscando como encontrar estos resultados, se indagan muchas posibles respuestas, se logra encontrar soluciones a partir de soluciones similares implementadas en ejercicios y tutoriales, entonces, como aporta esto al proceso de aprender para personas no técnicas?, Desarmar y armas, aplicar en otro contexto, es determinante para poder comprender como aprender de maneras diferentes, ahora tenemos la materia prima, pero debemos trabajar en la adaptación de esa materia prima, adaptar esa metodología, para poder acompañar a las personas en el aprendizajes de diferentes temas, sin dar el tema concreto, si no acompañando su búsqueda y su logro, es un camino no lineal

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,12 +4,24 @@ var espacio = d3.select("#viz")
         .attr("width", 1000)
         .attr("height", 500)    
 
+var dataset = ["data visualization", "nuevo ruralismo", "diseño", "identidad"]
+
+var cx = 700
+var cy = 300
 //circulo central
  var centro =  espacio.append("circle")
     	.attr("class", "middle-circle")
         .attr("r", 40)
-        .attr("cx", 700)
-        .attr("cy", 300);
+        .attr("cx", cx)
+        .attr("cy", cy);
+
+var angle = [];
+for (var i = 0; i < dataset.length; i++) {
+    var radian = Math.PI * 2 / dataset.length;
+    angle.push(radian * i)
+} 
+
+
 
 //circunsferencia
 var linea  = espacio.append("circle")
@@ -17,4 +29,7 @@ var linea  = espacio.append("circle")
 	.attr("r", 200)
 	.attr("cx", 700)
 	.attr("cy", 300);
+
+
+
 	
