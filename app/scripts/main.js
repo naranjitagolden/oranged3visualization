dff82d3616c0b2c63c91204c2f039b138cf385fa
naranjitagolden <naranja49@gmail.com>
2016-03-15 17:01:50 -0500

    Agregando imágen explorando comportamientos En este tag, se aprendieron muchas cosas, se buscó en google, poner imágen en circulo usando d3 y se encontró un ejemplo con JSfiddle, esta es la forma en que mejor se pueden encontrar soluciones, ya a la hora de implementarlo se cambiaron varias cosas de lugar, se mejoró el html, se cambió tambien el estilo de los circulos, y de acá en adelante se ubicó par imágen teniendo en cuenta que los valores de pattern afectan la legibilidad de la imágen

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,7 +12,7 @@ var espacio = d3.select("body")
         .append("g")
         .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");   
 
-var dataset = ["data visualization", "naturaleza", "creación estratégica", "identidad"]
+var dataset = ["data visualization", "naturaleza", "creación estratégica", "identidad", "humanismo", "tecnología"]
 
 var cx1 = 700
 var cx2 = 300
@@ -23,7 +23,15 @@ var cx2 = 300
     	.attr("class", "middle-circle")
         .attr("r", 50)
         .attr("cx", 0)
-        .attr("cy", 0);
+        .attr("cy", 0)
+        .on("mouseover", function(){ // when I use .style("fill", "red") here, it works 
+               d3.select(this)
+                   .style("fill", "url(#image)");
+         })
+          .on("mouseout", function(){ 
+               d3.select(this)
+                   .style("fill", "red");
+         });
 
 var angle = [];
 for (var i = 0; i < dataset.length; i++) {
@@ -33,37 +41,48 @@ for (var i = 0; i < dataset.length; i++) {
 
 var cx = angle.map(Math.cos)
 var cy = angle.map(Math.sin)
-
- var right =  espacio.append("circle")
-        .attr("class", "right-circle")
-        .attr("r", 40)
-        .attr("cx", (cx[0] * 200))
-        .attr("cy", cy[0]) ;
-
- var top =  espacio.append("circle")
-        .attr("class", "top-circle")
-        .attr("r", 40)
-        .attr("cx" , cx[1]) 
-        .attr("cy" , (cy[1] * -200)) ;
-
-var left =  espacio.append("circle")
-        .attr("class", "left-circle")
-        .attr("r", 40)
-        .attr("cx" , (cx[2] * 200)) 
-        .attr("cy" , cy[2]);
-
-var bottom =  espacio.append("circle")
-        .attr("class", "bottom-circle")
-        .attr("r", 40)
-        .attr("cx" , cx[3]) 
-        .attr("cy" , (cy[3] * -200));
+var rx = 200
+var ry = -200
 
 //circunsferencia
 var linea  = espacio.append("circle")
-	.attr("class", "around")
-	.attr("r", 200)
-	.attr("cx", 0)
-	.attr("cy", 0);
+    .attr("class", "around")
+    .attr("r", 200)
+    .attr("cx", 0)
+    .attr("cy", 0);
+
+ var right =  espacio.append("circle")
+        .attr("class", "all-circles")
+        .attr("r", 40)
+        .attr("cx", (cx[0] * rx))
+        .attr("cy", (cy[0] * ry));
+
+var another =  espacio.append("circle")
+        .attr("class", "all-circles")
+        .attr("r", 40)
+        .attr("cx" , (cx[1] * rx)) 
+        .attr("cy" , (cy[1] * ry));
+
+ var top =  espacio.append("circle")
+        .attr("class", "all-circles")
+        .attr("r", 40)
+        .attr("cx" , (cx[2] * rx)) 
+        .attr("cy" , (cy[2] * ry)) ;
+
+var left =  espacio.append("circle")
+        .attr("class", "all-circles")
+        .attr("r", 40)
+        .attr("cx" , (cx[3] * rx)) 
+        .attr("cy" , (cy[3] * ry));
+
+var bottom =  espacio.append("circle")
+        .attr("class", "all-circles")
+        .attr("r", 40)
+        .attr("cx" , (cx[4] * rx)) 
+        .attr("cy" , (cy[4] * ry));
+
+
+
 
 
 
