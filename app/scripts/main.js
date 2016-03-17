b81ba6799c4c4944db6414c47535f69b074e6440
naranjitagolden <naranja49@gmail.com>
2016-03-17 18:40:57 -0500

    #Please enter the commit message for your changes. Lines startin Explorando la generación dinámica de los circulos

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,10 @@
-// espacio svg
+// svg
 
-var espacio = d3.select('body');
 var width = 700,
     height = 600,
     radius = 50;
 
-var espacio = d3.select("body")
+var svg = d3.select("body")
         .append("svg")
         .attr("width", width)
         .attr("height", height)
@@ -19,7 +18,7 @@ var cx2 = 300
 
 
 //circulo central
- var centro =  espacio.append("circle")
+ var centro =  svg.append("circle")
     	.attr("class", "middle-circle")
         .attr("r", 50)
         .attr("cx", 0)
@@ -33,10 +32,15 @@ var cx2 = 300
                    .style("fill", "red");
          });
 
-var angle = [];
+//circunsferencia
+var linea  = svg.append("circle")
+    .attr("class", "around")
+    .attr("r", 200)
+    .attr("cx", 0)
+    .attr("cy", 0);
+
 for (var i = 0; i < dataset.length; i++) {
-    var radian = Math.PI * 2 / dataset.length;
-    angle.push(radian * i)
+    var angle = (Math.PI * 2 / dataset.length) * i;
 }; 
 
 var cx = angle.map(Math.cos)
@@ -44,42 +48,20 @@ var cy = angle.map(Math.sin)
 var rx = 200
 var ry = -200
 
-//circunsferencia
-var linea  = espacio.append("circle")
-    .attr("class", "around")
-    .attr("r", 200)
-    .attr("cx", 0)
-    .attr("cy", 0);
 
- var right =  espacio.append("circle")
-        .attr("class", "all-circles")
-        .attr("r", 40)
-        .attr("cx", (cx[0] * rx))
-        .attr("cy", (cy[0] * ry));
 
-var another =  espacio.append("circle")
-        .attr("class", "all-circles")
-        .attr("r", 40)
-        .attr("cx" , (cx[1] * rx)) 
-        .attr("cy" , (cy[1] * ry));
+ // var circles =  svg.selectAll("circle")
+ //        .dataset(dataset)
+ //        .enter()
+ //        .append("circle");
+ //        .attr("cx", function(d, i) {
+ //            return ()
+ //        })
+ //        .attr("r", 40)
+ //        .attr("cx", (cx[0] * rx))
+ //        .attr("cy", (cy[0] * ry));
 
- var top =  espacio.append("circle")
-        .attr("class", "all-circles")
-        .attr("r", 40)
-        .attr("cx" , (cx[2] * rx)) 
-        .attr("cy" , (cy[2] * ry)) ;
 
-var left =  espacio.append("circle")
-        .attr("class", "all-circles")
-        .attr("r", 40)
-        .attr("cx" , (cx[3] * rx)) 
-        .attr("cy" , (cy[3] * ry));
-
-var bottom =  espacio.append("circle")
-        .attr("class", "all-circles")
-        .attr("r", 40)
-        .attr("cx" , (cx[4] * rx)) 
-        .attr("cy" , (cy[4] * ry));
 
 
 
