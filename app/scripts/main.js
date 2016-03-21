9d47f9e1ed49ce0303e053bc622a085d7896815d
naranjitagolden <naranja49@gmail.com>
2016-03-21 07:48:48 -0500

    Organizando el código usando puglins para sublime como JsHint https://github.com/uipoet/sublime-jshint y javascript format, o js beautify https://github.com/beautify-web/js-beautify se organiza el código para que quede este máslegible y no duelan los ojos :D ahora a continuar engallando el código, vamos a averiguar donde están los circulos que faltan, y tambien a ponerle el texto de los datos, y más adelante ver como cargar los datos específicos

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,63 +1,50 @@
-
 var w = 700,
     h = 600;
 
 var svg = d3.select("body")
-        .append("svg")
-        .attr("width", w)
-        .attr("height", h)
-        .append("g")
-        .attr("transform", "translate(" + w / 2 + "," + h / 2 + ")");   
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h)
+    .append("g")
+    .attr("transform", "translate(" + w / 2 + "," + h / 2 + ")");
 
 var dataset = ["tecnología", "identidad", "humanismo", "naturaleza", "creación Audiovisual", "hola"];
 
 //circulo central
- var centro =  svg.append("circle")
-    	.attr("class", "middle-circle")
-        .attr("r", 50)
-        .attr("cx", 0)
-        .attr("cy", 0)
-        .on("mouseover", function(){ // when I use .style("fill", "red") here, it works 
-               d3.select(this)
-                   .style("fill", "url(#image)");
-         })
-          .on("mouseout", function(){ 
-               d3.select(this)
-                   .style("fill", "red");
-         });
+var centro = svg.append("circle")
+    .attr("class", "middle-circle")
+    .attr("r", 50)
+    .attr("cx", 0)
+    .attr("cy", 0)
+    .on("mouseover", function() { // when I use .style("fill", "red") here, it works 
+        d3.select(this)
+            .style("fill", "url(#image)");
+    })
+    .on("mouseout", function() {
+        d3.select(this)
+            .style("fill", "red");
+    });
 
 //circunsferencia
-var linea  = svg.append("circle")
+var linea = svg.append("circle")
     .attr("class", "around")
     .attr("r", 200)
     .attr("cx", 0)
     .attr("cy", 0);
 
-var rar = Math.PI*2 / dataset.length
+var rar = Math.PI * 2 / dataset.length;
 
-var circles =  svg.selectAll("circle")
-        .data(dataset)
-        .enter()
-        .append("g")
-        .append("circle");
+var circles = svg.selectAll("circle")
+    .data(dataset)
+    .enter()
+    .append("circle");
 
-circles.attr("cx", function(d, i){
-        var angle = i * rar;
-        return Math.cos(angle) * 200;
-       })
-       .attr("cy", function(d,i){
+circles.attr("cx", function(d, i) {
+    var angle = i * rar;
+    return Math.cos(angle) * 200;
+})
+    .attr("cy", function(d, i) {
         var angle = i * rar;
         return Math.sin(angle) * -200;
-       })
-       .attr("r", 50);
-
-
-
-
-
-
-
-
-
-
-	
+    })
+    .attr("r", 50);
